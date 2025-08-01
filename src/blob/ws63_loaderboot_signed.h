143d056b4f5d1e07c72bc1df533d293e6d0dc84b
Gong Zhile <gongzl@stu.hebust.edu.cn>
2025-08-01 18:02:41 +0800

    src/blob: add ws63 loaderboot blob

---
+++ src/blob/ws63_loaderboot_signed.h
@@ -0,0 +1,11 @@
+/*
+  WS63 LiteOS signed bootloader blob (root_loaderboot_sign.bin)
+*/
+
+#ifndef _BLOB_WS63_LOADERBOOT_SIGNED_H_
+#define _BLOB_WS63_LOADERBOOT_SIGNED_H_
+
+extern const unsigned char ws63_loaderboot_signed_bin[];
+extern const unsigned int ws63_loaderboot_signed_len;
+
+#endif	/* _BLOB_WS63_LOADERBOOT_SIGNED_H_ */
